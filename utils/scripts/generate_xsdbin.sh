88ce386399c5ec214356d8a09f01897034f0c724
Carlos A. Wong <carloswong54@gmail.com>
2022-07-08 01:04:58 -0400

    generate xsdbin script first pass

--- utils/scripts/generate_xsdbin.sh
+++ utils/scripts/generate_xsdbin.sh
@@ -5,4 +5,18 @@ for _ in {1,2,3}; do
   workspace_dir=$(dirname ${workspace_dir})
 done
 
-xsdbin_x86_paths=$(find ${workspace_dir} -name xsdbin -path cmake*x86)
+xsdbin_x86_paths=$(find ${workspace_dir} -name xsdbin -path '*cmake*x86*')
+
+readarray -t xsdbin_x86_paths_array <<< "${xsdbin_x86_paths}"
+
+for xsdbin in "${xsdbin_x86_paths_array[@]}"; do
+  suffix="-schema-32"
+  if [[ "${xsdbin}" == *"x86_64"* ]]; then
+    suffix="-schema-64"
+  fi
+  cmd="${xsdbin} --hxx-suffix "${suffix}.hxx" --cxx-suffix "${suffix}.cxx" --output-dir ${workspace_dir}/include ${workspace_dir}/library.xsd"
+  echo ${cmd}
+  $(${cmd})
+done
+
+mv ${workspace_dir}/include/*-schema*.cxx ${workspace_dir}/src
